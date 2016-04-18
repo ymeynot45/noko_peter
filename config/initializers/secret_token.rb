9d1a2281984dc726034b735c1b11ff7acf2d74ad
William Karsten <yme_ynot45@hotmail.com>
2016-04-18 10:33:20 -0500

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NokoPeter::Application.config.secret_key_base = '312297e535d2fbb80667e0ef5d04c5b72996dca3614498afe4636e61a8c88e67b8de3c6df8720d17788b12e8648118346d990c3998eee39aeda8b3df41d3f79c'
