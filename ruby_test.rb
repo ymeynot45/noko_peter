c237efcf05380587f5bd16f50bd1435a33354ab6
William Karsten <yme_ynot45@hotmail.com>
2016-04-19 11:57:40 -0500

    indentifying the links

--- ruby_test.rb
+++ ruby_test.rb
@@ -1,3 +1,12 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+
+url = "http://www.softwarebyrob.com/archives/"
+doc = nokogiri::HTML(open(url))
+puts doc.at_css("title").text
+
+doc.css("ul:nth-child(4) a").each do |item|
+  puts item.at_css(".prodLink").text
+end
+
